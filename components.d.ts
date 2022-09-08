8c334cde2ca40fc3a00e6a2b52ed5cf1eaf261dc
SmallCoal5 <71584575+SmallCoal5@users.noreply.github.com>
2022-09-08 20:36:27 +0800

    fix: 实现登录和注册功能呢

--- components.d.ts
+++ components.d.ts
@@ -41,5 +41,6 @@ declare module "@vue/runtime-core" {
     VanTabbarItem: typeof import("vant/es")["TabbarItem"];
     VanTabs: typeof import("vant/es")["Tabs"];
     VanTag: typeof import("vant/es")["Tag"];
+    VanUploader: typeof import("vant/es")["Uploader"];
   }
 }
